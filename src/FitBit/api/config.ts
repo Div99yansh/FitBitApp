0f3420f05ad7c19377906e787304770664559889
Div99yansh <gdivyanshgoyal99@outlook.com>
2026-01-24 03:39:54 +0530

    Add env url for deployment

--- src/FitBit/api/config.ts
+++ src/FitBit/api/config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://127.0.0.1:8000";
+export const API_BASE_URL = import.meta.env.FITBITTRACKER_API_BASE_URL || "http://127.0.0.1:8000";
 export const USE_MOCK_DATA = false; // Set to false to use real API
